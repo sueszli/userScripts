6a4ccb177b4c119dae2efdb4c8548c8f5193b5e4
sueszli <jabaryyahya@gmail.com>
2022-05-26 00:31:40 +0200

    init

---
+++ fuckboy-messenger.user.js
@@ -0,0 +1,189 @@
+// ==UserScript==
+// @name            Fuckboy Messenger
+// @namespace       sueszli
+// @match           *://*web.whatsapp.com/*
+// @grant           none
+// @version         1.0
+// @author          sueszli
+// @description     Automatize the daily fuckboy business! (created as a joke for a friend)
+// ==/UserScript==
+
+// also see: https://greasyfork.org/en/scripts/36066-whatsapp-web-spammer/code
+
+// ======================================== DATA ========================================
+let data = [
+    "Yeah that really sucks... or doesn't - don't know. Anyway do you know anyone that's down for a threesome?",
+    "Can I just be honest with you for a second? This whole conversation is just a joke, right? I'm not even sure if I'm actually interested in you",
+    "I know exactly how to treat you right - just give me a call and I'll be right there whenever you feel like it",
+    "So what's cooking, good-looking? Wyd tonight? I'm in the mood for a good time",
+    "You're just overthinking everything. I'm not even sure what I meant by that",
+    "So this is coming out of nowhere but I've been thinking about you a lot recently - you catch my drift?",
+    "Man I wish I was in Berlin right now ...",
+    "I pursue beautiful women and even more beautiful poetry - what is the last book you read?",
+    "Yeah I'm super into techno and even more into technology - have you heard of NFT's before?",
+    "Aha you remind me of my ex - she was so hot. I mean, I don't know if I can say that, but I think she was a great friend - and she still is to me",
+    "Idk maybe i romanticise but you have a special vibe that I can't get enough of aha. Anyway wanna listen to mac demarco at mine? <3",
+    "Yo it's not that deep - I'm just living in the moment - I'm not sure if I'm in love with you - but I love having a good time with you aha",
+    "Yeah I'd consider myself an artist - I know a lot of art - I know how to appreciate beauty",
+];
+// ========================================================================================
+
+
+// util ::
+/**
+ * Waits for an element satisfying selector to exist, then resolves promise with the element which it then returns.
+ */
+function elementReady(xPath) {
+    return new Promise((resolve, reject) => {
+        const el = getNodes(xPath);
+        // found immediately
+        if (el) {
+            resolve(el);
+        }
+        // wait for it to appear
+        new MutationObserver((mutationRecords, observer) => {
+            // Query for elements matching the specified path
+            if (getNodes(xPath).length === 0) {
+                resolve(getNodes(xPath));
+                observer.disconnect();
+            }
+        }).observe(document.documentElement, {
+            childList: true, subtree: true
+        });
+    });
+}
+
+/**
+ *  Returns an array of nodes matching the xPath expression.
+ */
+function getNodes(xpath) {
+    let nodes = [];
+    let result = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+    let node = result.iterateNext();
+    while (node) {
+        nodes.push(node);
+        node = result.iterateNext();
+    }
+    return nodes;
+}
+
+/**
+ * Returns the first node matching the xPath expression.
+ */
+function getFirstNode(xPath) {
+    return getNodes(xPath)[0];
+}
+// :: util
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// features ::
+function createMessageButton() {
+    // button already exists
+    const button = document.getElementById('messageButton');
+    let chatBar = getFirstNode("//body/div[@id='app']/div[1]/div[1]/div[4]/div[1]/footer[1]/div[1]");
+    if(button || !chatBar) {
+        return;
+    }
+
+    // create eggplant button
+    let b = document.createElement('button');
+    b.setAttribute("id", "messageButton");
+    b.innerHTML = '🍆';
+    b.style.fontSize = '120%';
+    b.style.padding = '0.5rem 0.5rem 1rem 1.4rem';
+    chatBar.append(b);
+    b.onclick = () => {
+        insertMessage();
+    }
+}
+
+function changeChatName() {
+    // set all contact names to jaqueline
+    const nameElem = getFirstNode("//header/div[2]/div[1]/div[1]/span[1]");
+    nameElem.innerHTML = "Jaqueline Ferguson";
+}
+
+function insertMessage() {
+    let container1 = getFirstNode("//body/div[@id='app']/div[1]/div[1]/div[4]/div[1]/footer[1]/div[1]/div[1]/span[2]/div[1]/div[2]/div[1]/div[1]")
+    container1.classList.add("focused");
+
+    let container2 = getFirstNode("//div[contains(text(),'Type a message')]");
+    container2.style.visibility = "hidden";
+
+    // write into text field
+    let textField = getFirstNode("//body/div[@id='app']/div[1]/div[1]/div[4]/div[1]/footer[1]/div[1]/div[1]/span[2]/div[1]/div[2]/div[1]/div[1]/div[2]");
+    textField.innerHTML = data[Math.floor(Math.random() * data.length)];
+    textField.focus();
+    let evt = new Event('input', {
+        bubbles: true
+    });
+    textField.dispatchEvent(evt);
+
+    // "ENTER" keyboard key event to send message
+    let event_Enter = new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        key: 'Enter',
+        code: 'Enter',
+        keyCode: 13,
+        charCode: 13
+    });
+    textField.dispatchEvent(event_Enter);
+}
+
+function setComicSans() {
+    let head = document.getElementsByTagName ('head')[0];
+    let s = document.createElement ('style');
+    s.setAttribute ('type', 'text/css');
+
+    if (s.styleSheet) {
+        s.styleSheet.cssText = "body { font-family: \"Comic Sans MS\"!important; } ";
+    } else {
+        s.appendChild (document.createTextNode ("body { font-family: \"Comic Sans MS\"!important; } "));
+    }
+    head.appendChild (s);
+}
+// :: features
+
+
+// main ::
+console.clear();
+// setComicSans();
+
+document.onclick = () => {
+    console.log("Clicked");
+    createMessageButton();
+    // changeChatName();
+    // setComicSans();
+};
+// :: main
---
+++ whatsapp-chats.user.js
@@ -0,0 +1,155 @@
+// ==UserScript==
+// @name            WhatsApp Chats
+// @namespace       sueszli
+// @match           *://*web.whatsapp.com/*
+// @grant           none
+// @version         1.0
+// @author          sueszli
+// @description     Simple functions to read chat elements from active and archived chats.
+// ==/UserScript==
+
+
+// ======================================== CONFIG ========================================  
+let selectedChats = ["Test 0", "Test 1", "Test 2"];
+
+// ========================================================================================
+
+
+// util ::
+/**
+ * Waits for logout button to disappear (then page is fully loaded)
+ */
+function applicationReady() {
+    const logOutButtonPath = "//body/div[@id='app']/div[1]/div[1]/div[5]/div[1]";
+
+    return new Promise((resolve, reject) => {
+        const el = getNodes(logOutButtonPath);
+        // loaded immediately
+        if (!el) {
+            resolve(el);
+        }
+        // wait for it to disappear
+        new MutationObserver((mutationRecords, observer) => {
+            // Query for elements matching the specified selector
+            const e = getNodes(logOutButtonPath);
+            if (e.length === 0) {
+                resolve(getNodes(logOutButtonPath));
+                observer.disconnect();
+            }
+        }).observe(document.documentElement, {
+            childList: true, subtree: true
+        });
+    });
+}
+
+/**
+ * Waits for an element satisfying selector to exist, then resolves promise with the element which it then returns.
+ */
+function elementReady(xPath) {
+    return new Promise((resolve, reject) => {
+        const el = getNodes(xPath);
+        // found immediately
+        if (el) {
+            resolve(el);
+        }
+        // wait for it to appear
+        new MutationObserver((mutationRecords, observer) => {
+            // Query for elements matching the specified path
+            if (getNodes(xPath).length === 0) {
+                resolve(getNodes(xPath));
+                observer.disconnect();
+            }
+        }).observe(document.documentElement, {
+            childList: true, subtree: true
+        });
+    });
+}
+
+/**
+ *  Returns an array of nodes matching the xPath expression.
+ */
+function getNodes(xpath) {
+    let nodes = [];
+    let result = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+    let node = result.iterateNext();
+    while (node) {
+        nodes.push(node);
+        node = result.iterateNext();
+    }
+    return nodes;
+}
+
+/**
+ * Returns the first node matching the xPath expression.
+ */
+function getFirstNode(xPath) {
+    return getNodes(xPath)[0];
+}
+// :: util
+
+
+// reader ::
+/**
+ * Reads chat elements from active chats
+ */
+function getLastChats() {
+    // get active chats
+    let sidePanel = getFirstNode("//body/div[@id='app']/div[1]/div[1]/div[3]/div[1]/div[2]/div[2]/div[1]/div[1]");
+    let chatElems = sidePanel.children;
+
+    // filter
+    for (let i = 0; i < chatElems.length; i++) {
+        let contactName = chatElems[i].querySelector("[role='gridcell'] span").innerText;
+        if (selectedChats.includes(contactName)) {
+            console.log("%c[selected] Active Chat: " + contactName, "background: #222; color: #bada55");
+        } else {
+            console.log("Active Chat: " + contactName);
+        }
+    }
+}
+
+/**
+ * Reads chat elements from archived chats
+ */
+function getArchivedChats() {
+    // open archived chats panel
+    getFirstNode("//body/div[@id='app']/div[1]/div[1]/div[3]/div[1]/div[2]/button[1]").click();
+
+    // wait for archived chats to load
+    const sidePanelPath = "//body/div[@id='app']/div[1]/div[1]/div[2]/div[1]/span[1]/div[1]/span[1]/div[1]/div[1]/div[1]/div[1]/div[1]";
+    elementReady(sidePanelPath).then(() => {
+        let sidePanel = getFirstNode(sidePanelPath);
+        let chatElems = sidePanel.children;
+
+        // filter
+        for (let i = 0; i < chatElems.length; i++) {
+            let contactName = chatElems[i].querySelector("[role='gridcell'] span").innerText;
+            if (selectedChats.includes(contactName)) {
+                console.log("%c[selected] Archived Chat: " + contactName, "background: #222; color: #bada55");
+            } else {
+                console.log("Archived Chat: " + contactName);
+            }
+        }
+
+        // close archived chats panel
+        getFirstNode("//header/div[1]/div[1]/button[1]").click();
+    })
+}
+// :: reader
+
+
+// main ::
+(function () {
+    'use strict';
+
+    // wait for DOM to load and the 'logout' button to disappear
+    window.addEventListener("load", () => {
+        applicationReady().then(() => {
+            console.clear();
+            console.log("Starting script...\n");
+            getLastChats();
+            getArchivedChats();
+        });
+    });
+})();
+// :: main
